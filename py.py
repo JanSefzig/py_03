55e2d406edab2730cb9dc468337430a24b1a64ed
JanSefzig <nicolas.jan.sefzig@student.ossp.cz>
2022-09-27 10:19:56 +0200

    done

---
+++ py.py
@@ -0,0 +1,20 @@
+arr = ['d', 'a', 't', 'e','l']
+print(arr)
+
+result=list(reversed(arr))
+print(result)
+
+num = 1456
+print(str(num)[::-1])
+
+b = "Řetezce se nechovají jako seznamy"
+
+x = b.replace("nechovají", "chovají")
+
+print(x) 
+
+
+a = "Ramanujan byl velmi zajímavý geniální indický matematik"
+slova = a.split()
+pocet_slov = len(slova)
+print(pocet_slov)
